66e19ec51d50d4f95942188297eca94fe282315a
sudodeo <adesolaadeoluwa@gmail.com>
2024-05-20 04:45:54 +0100

    tidy up file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("Task Track Pro API")
+    .setDescription("API for managing tasks")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
