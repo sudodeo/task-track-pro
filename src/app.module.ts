aa454af8ef88be5df343e8afab14f9884dfa4bbb
sudodeo <adesolaadeoluwa@gmail.com>
2024-05-20 04:43:35 +0100

    update module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { DatabaseModule } from "./database/database.module";
+import { TaskModule } from "./tasks/task.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [".env", ".env.production"],
+    }),
+    DatabaseModule,
+    TaskModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
